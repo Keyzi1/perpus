da8ab358debc990ead3f9d5b67f77e33bff412a4
Keyzi-Muhadzibtyaga <126256084+KeyziMuhadzibtyaga@users.noreply.github.com>
2024-04-28 14:11:28 +0700

    update

--- application/views/Peminjaman/peminjaman_view.php
+++ application/views/Peminjaman/peminjaman_view.php
@@ -26,12 +26,8 @@
 <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
         <header class="main-header">
-            <!-- Logo -->
-            <a href="index2.html" class="logo">
-                <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>A</b>ip</span>
-                <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>Parsat</b>Perpus</span>
+            <a href="<?php echo base_url() ?>dashboard" class="logo">
+                <span class="logo-lg">Perpustakaan</span>
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
